41240157c517c008b140f235ea8a0d2ff79c46d5
pranaybhosale1124 <pranaybhosale1124@gmail.com>
2024-07-02 10:07:28 +0530

    bug fix

--- routes/chat.js
+++ routes/chat.js
@@ -5,14 +5,16 @@ const sequelize = require('../models/db-config')
 var initModels = require("../models/init-models");
 const { getChatKey } = require('../service/chat-service')
 var models = initModels(sequelize);
+const { storeChat, updateChat, getChat } = require('../bin/cache-store')
 
 router.post('/get-chat', async (req, res) => {
     let currentUser = req.body.me;
     let user = req.body.user;
     let chat_id = await getChatKey(currentUser, user);
 
-    const chat = await models.chat.findOne({ where: { chat_id }, attributes: ['chat_data'] });
-
+    let chat = await getChat(chat_id);
+    if (!chat)
+        chat = await models.chat.findOne({ where: { chat_id }, attributes: ['chat_data'] });
     if (chat) {
         res.status(200).json({ chat_data: chat.chat_data });
     } else {
